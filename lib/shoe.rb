cecc0e4da715ac069fa1522a97960265905145d0
Jane Justiz <janejustiz@gmail.com>
2018-06-30 18:41:58 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,6 +10,7 @@ class Shoe
       if word == @brand
       else
         BRANDS << @brand
+      end
     end
   end
 
@@ -17,5 +18,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
