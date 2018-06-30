987d78930ad2a9eec9e66ff3cf3d057d3e778744
Jane Justiz <janejustiz@gmail.com>
2018-06-30 18:50:34 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    if BRANDS.any?{|x| x == @brand} == true
+    if BRANDS.any?{|x| x == brand} == true
       BRANDS.delete(brand)
     end
   end
